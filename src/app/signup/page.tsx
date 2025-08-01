aa3b73a88858b205f220eb306d4f341022fc643e
Johan Meneses <johan.andres.meneses@gmail.com>
2025-08-01 19:45:12 +0000

    Error: Element type is invalid: expected a string (for built-in componen

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useRouter, Link } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
